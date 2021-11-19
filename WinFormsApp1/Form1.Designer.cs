f03fc4892fc7bb9acbd83a59d212dba13b4baac3
Hekzagonal <Hekzagonal@DESKTOP-PS2C8QE>
2021-11-19 09:47:32 +0700

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFaktorial = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.txtHasil = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFaktorial
+            // 
+            this.txtFaktorial.Location = new System.Drawing.Point(88, 35);
+            this.txtFaktorial.Name = "txtFaktorial";
+            this.txtFaktorial.Size = new System.Drawing.Size(107, 23);
+            this.txtFaktorial.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Faktorial dari";
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.Location = new System.Drawing.Point(60, 81);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(75, 23);
+            this.btnHitung.TabIndex = 3;
+            this.btnHitung.Text = "Hitung";
+            this.btnHitung.UseVisualStyleBackColor = true;
+            this.btnHitung.Click += new System.EventHandler(this.btnHitung_Click);
+            // 
+            // txtHasil
+            // 
+            this.txtHasil.Location = new System.Drawing.Point(12, 148);
+            this.txtHasil.Multiline = true;
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.Size = new System.Drawing.Size(183, 196);
+            this.txtHasil.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Deskripsi";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(60, 365);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 400);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHasil);
+            this.Controls.Add(this.btnHitung);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFaktorial);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtFaktorial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHitung;
+        private System.Windows.Forms.TextBox txtHasil;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1 
+{ 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            int no, faktorial;
+
+            no = Convert.ToInt32(txtFaktorial.Text);
+            int hasil = no;
+
+            for (int i = no-1; i > 0; i--)
+            {
+                faktorial = hasil;
+                hasil *= i;
+                
+                txtHasil.Text += Convert.ToString(faktorial + " * " + i + " = " + hasil) + "\r\n";
+            }
+            txtHasil.Text += Convert.ToString("Hasil Faktorial Dari " + no + " Adalah " + hasil);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtHasil.Clear();
+        }
+    }
+}
